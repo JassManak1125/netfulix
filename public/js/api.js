85970ed4c1eddf31d594ba21f9ac72ead23d2179
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2022-01-20 14:39:44 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "bc6feab101c93ca608181f9e66b049f3";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
